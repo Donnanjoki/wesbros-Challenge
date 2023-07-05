02c1c9f6f2a1e8a61d26778d183031f32f884afa
Donna Njoki <njerinjoki2@gmail.com>
2023-07-05 17:28:54 +0300

    debugging typos

--- Javascript Drum Kit/app.js
+++ Javascript Drum Kit/app.js
@@ -1,24 +1,17 @@
-function playSound(e){
-    // selects the audio element where data-key="e.keyCode"
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  
-    //if there is no audio element with the corresponding keyCode, then don't do anything
-    if(!audio) return;
-    audio.currentTime = 0
-    audio.play()
-    
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-  
-    key.classList.add('playing')
-  }
-  
-  function removeTransition(e){
-    //transitionend is triggered on a variety of properties, (border, box shadow, etc)
-    // just select one - in this case 'transform'
-    if(e.propertyName != 'transform') return;
-    e.target.classList.remove('playing')
-  }
-  
-  const keys = document.querySelectorAll(".key")
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-  window.addEventListener('keydown', playSound)
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stop function from running all together
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip it if it is not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
