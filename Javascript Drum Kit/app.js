0d46020c18e3f3ffefed7fa5dd80343b1b8eb5c6
Donna Njoki <njerinjoki2@gmail.com>
2023-07-10 23:22:23 +0300

    functionality works!

--- Javascript Drum Kit/app.js
+++ Javascript Drum Kit/app.js
@@ -17,6 +17,27 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 
-// add click functionality
+// to add click functionality
 
+function playSound(e) {
+    let code;
+    if (e.keyCode) {
+      // it was a keypress, get the keycode as usual
+      code = e.keyCode;
+    } else {
+      // it was a click.  Read the keycode from the div that was clicked
+      code = this.dataset.key;
+    }
+  
+    const audio = document.querySelector(`audio[data-key="${code}"]`);
+    const key = document.querySelector(`.key[data-key="${code}"]`);
+  
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
 
+for (let i=0, key; key = keys[i]; i++) {
+    key.addEventListener('click', playSound);
+}
